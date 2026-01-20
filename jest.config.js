eab86fa44e98802ea70c44a38870e97113a36f22
Matt Rajkowski <sourcecoder@zeroio.com>
2026-01-19 19:58:03 -0500

    Updated javascript dependencies and test framework

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/*.(test|spec).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }],
+  },
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+  testTimeout: 10000
+};
---
+++ src/test/setup.ts
@@ -0,0 +1,7 @@
+// Jest setup file for tests
+import '@testing-library/jest-dom';
+
+// Global test setup
+beforeEach(() => {
+  // Reset any global state before each test
+});
