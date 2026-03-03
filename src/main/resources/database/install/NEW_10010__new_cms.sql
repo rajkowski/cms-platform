5abb79310b75ba867849ecc05c1aeb8375468a06
Matt Rajkowski <sourcecoder@zeroio.com>
2026-03-02 22:32:43 -0500

    Add field

--- src/main/resources/database/install/NEW_10010__new_cms.sql
+++ src/main/resources/database/install/NEW_10010__new_cms.sql
@@ -712,6 +712,7 @@ CREATE TABLE site_exports (
   export_date TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
   export_by BIGINT REFERENCES users(user_id),
   export_path VARCHAR(255) NOT NULL,
+  export_size BIGINT DEFAULT 0,
   notes TEXT
 );
 CREATE INDEX site_exports_dt_idx ON site_exports(export_date);
